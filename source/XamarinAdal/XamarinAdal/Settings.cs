699651553ec2d8c0a8971ed3a1b5464784cd5c52
Roberto Cervantes <rcervantes@outlook.com>
2017-11-03 20:04:46 -0500

    Initial commit

--- source/XamarinAdal/XamarinAdal/Settings.cs
+++ source/XamarinAdal/XamarinAdal/Settings.cs
@@ -4,9 +4,9 @@ namespace XamarinAdal
     public class Settings
     {
         public static string AppServiceURL = "https://APP_SERVICE.azurewebsites.net/";
-        public static string TenantId = "https://login.windows.net/common"; // the ID of the AD Tenant
-        public static string ResourceId = "RESOURCE_ID"; // the ID of the Resource you are accessing
-        public static string ClientId = "CLIENT_ID"; // the ID of the THIS client app (Native App registration)
-        public static string ReturnUrl = $"{Settings.AppServiceURL}.auth/login/done"; // whatever returnURL's you have configured
+        public static string TenantId = "https://login.windows.net/common";
+        public static string ResourceId = "RESOURCE_ID";
+        public static string ClientId = "CLIENT_ID";
+        public static string ReturnUrl = $"{Settings.AppServiceURL}.auth/login/done";
     }
 }
